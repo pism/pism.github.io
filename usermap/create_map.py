f367f9129a42fdb5c13a61b88a3dd42852a82e04
m-kreuzer <kreuzer@pik-potsdam.de>
2021-05-25 18:16:18 +0200

    testing first version of pism usermap

--- usermap/create_map.py
+++ usermap/create_map.py
@@ -3,49 +3,59 @@
 # based on https://github.com/academicpages/academicpages.github.io/blob/master/talkmap.py
 # (c) 2016-2017 R. Stuart Geiger, released under the MIT license
 # 
-# Run this from the usermap/ directory, which contains .md files of all your talks. This scrapes the location YAML field from each .md file, geolocates it with geopy/Nominatim, and uses the getorg library to output data, HTML, and Javascript for a standalone cluster map.
+# Run this from the usermap/ directory, which contains the file pism_users.csv This scrapes the city of each group, geolocates it with geopy/Nominatim, and uses the getorg library to output data, HTML, and Javascript for a standalone cluster map.
 # 
-# Requires: glob, getorg, geopy
+# Requires: getorg, geopy
 #!pip install getorg python-frontmatter
 
-import glob
 import getorg
 from geopy import Nominatim
+import csv
 
-g = glob.glob("_talks/*.md")
+input_file = 'pism_users.csv'
 
-geocoder = Nominatim(user_agent="my-application")
+#geocoder = Nominatim(user_agent="my-application")
+geocoder = Nominatim(user_agent='test')
 location_dict = {}
 location = ""
-permalink = ""
-title = ""
-venue = ""
-count=0
 
-for file in g:
-    with open(file, 'r') as f:
-        lines = f.read()
-        if lines.find('location: "') > 1:
-            loc_start = lines.find('location: "') + 11
-            lines_trim = lines[loc_start:]
-            loc_end = lines_trim.find('"')
-            location = lines_trim[:loc_end]
-        if lines.find('title: "') > 1:
-            loc_start = lines.find('title: "') + 8
-            lines_trim = lines[loc_start:]
-            loc_end = lines_trim.find('"')
-            title = lines_trim[:loc_end]
-        if lines.find('venue: "') > 1:
-            loc_start = lines.find('venue: "') + 8
-            lines_trim = lines[loc_start:]
-            loc_end = lines_trim.find('"')
-            venue = lines_trim[:loc_end]
 
-        key = str(title + " | " + venue + ", " + location)
+with open(input_file, 'r') as f:
+	csv_data = csv.DictReader(f, delimiter=',')
+	for row in csv_data:
+		#print(row)
 
-        location_dict[key] = geocoder.geocode(location)
-        print(key, "\n", location_dict[key])
-    count = count + 1
+		key = f'{row["name"]} | {row["city"]}, {row["country"]} | {row["website"]}'
+		location = f'{row["city"]}, {row["country"]}'
+		location_dict[key] = geocoder.geocode(location)
+
+print(location_dict)
+	
+		
+#for file in g:
+#    with open(file, 'r') as f:
+#        lines = f.read()
+#        if lines.find('location: "') > 1:
+#            loc_start = lines.find('location: "') + 11
+#            lines_trim = lines[loc_start:]
+#            loc_end = lines_trim.find('"')
+#            location = lines_trim[:loc_end]
+#        if lines.find('title: "') > 1:
+#            loc_start = lines.find('title: "') + 8
+#            lines_trim = lines[loc_start:]
+#            loc_end = lines_trim.find('"')
+#            title = lines_trim[:loc_end]
+#        if lines.find('venue: "') > 1:
+#            loc_start = lines.find('venue: "') + 8
+#            lines_trim = lines[loc_start:]
+#            loc_end = lines_trim.find('"')
+#            venue = lines_trim[:loc_end]
+#
+#        key = str(title + " | " + venue + ", " + location)
+#
+#        location_dict[key] = geocoder.geocode(location)
+#        print(key, "\n", location_dict[key])
+#    count = count + 1
 
 m = getorg.orgmap.create_map_obj()
-getorg.orgmap.output_html_cluster_map(location_dict, folder_name="talkmap/", hashed_usernames=False)
+getorg.orgmap.output_html_cluster_map(location_dict, folder_name="./", hashed_usernames=False)
