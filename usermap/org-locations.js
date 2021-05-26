8166acca10f1e4accd1a0f88f03649b98335c8ae
m-kreuzer <kreuzer@pik-potsdam.de>
2021-05-26 19:33:14 +0200

    adding north american PISM users

--- usermap/org-locations.js
+++ usermap/org-locations.js
@@ -1,12 +1,52 @@
 var addressPoints = [
   [
-    "UAF | Fairbanks, USA | <a href='https://glaciers.gi.alaska.edu/' target='_blank'>https://glaciers.gi.alaska.edu/</a>",
+    "Harvard University | Cambridge, USA | <a href='?' target='_blank'>?</a>",
+    42.3750997,
+    -71.1056157
+  ],
+  [
+    "Louisina State University | Baton Rouge, USA | <a href='?' target='_blank'>?</a>",
+    30.4459596,
+    -91.18738
+  ],
+  [
+    "Pennsylvania State University | State Park, USA | <a href='https://www.psice.psu.edu/ ' target='_blank'>https://www.psice.psu.edu/ </a>",
+    42.4794133,
+    -104.9702482
+  ],
+  [
+    "Potsdam Institute for Climate Impact Research (PIK) | Potsdam, Germany | <a href='https://www.pik-potsdam.de/ice' target='_blank'>https://www.pik-potsdam.de/ice</a>",
+    52.4009309,
+    13.0591397
+  ],
+  [
+    "University of Alaska Fairbanks (UAF) | Fairbanks, USA | <a href='https://glaciers.gi.alaska.edu/' target='_blank'>https://glaciers.gi.alaska.edu/</a>",
     64.837845,
     -147.716675
   ],
   [
-    "PIK | Potsdam, Germany | <a href='https://www.pik-potsdam.de/ice' target='_blank'>https://www.pik-potsdam.de/ice</a>",
-    52.4009309,
-    13.0591397
+    "University of Chicago (UC) | Chicago, USA | <a href='https://geosci.uchicago.edu/research/atmospheres-oceans-ice-and-climate/' target='_blank'>https://geosci.uchicago.edu/research/atmospheres-oceans-ice-and-climate/</a>",
+    41.8755616,
+    -87.6244212
+  ],
+  [
+    "University of Maine | Orono, USA | <a href='https://umaine.edu/earthclimate/research/climate-change/' target='_blank'>https://umaine.edu/earthclimate/research/climate-change/</a>",
+    44.883607,
+    -68.672791
+  ],
+  [
+    "University of Northern British Columbia (UNBC) | Prince George, Canada | <a href='?' target='_blank'>?</a>",
+    53.9128645,
+    -122.7453699
+  ],
+  [
+    "University of Washingtion (UW) | Seattle, USA | <a href='https://earthweb.ess.washington.edu/Glaciology/ ' target='_blank'>https://earthweb.ess.washington.edu/Glaciology/ </a>",
+    47.6038321,
+    -122.3300624
+  ],
+  [
+    "Yale University | New Haven, USA | <a href='?' target='_blank'>?</a>",
+    41.298434349999994,
+    -72.93102342707913
   ]
 ];
