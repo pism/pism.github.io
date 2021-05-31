69b4301219b1c2fcbb7f6cd2fd18b66aed2318c0
m-kreuzer <kreuzer@pik-potsdam.de>
2021-05-31 20:02:35 +0200

    added Asian and Australian groups

--- usermap/org-locations.js
+++ usermap/org-locations.js
@@ -5,14 +5,24 @@ var addressPoints = [
     -71.1056157
   ],
   [
-    "Louisina State University | Baton Rouge, USA",
+    "Korea Polar Research Institute (KOPRI) | Incheon, South Korea | <a href='https://eng.kopri.re.kr/eng/' target='_blank'>https://eng.kopri.re.kr/eng/</a>",
+    37.456,
+    126.7052
+  ],
+  [
+    "Louisina State University (LSU) | Baton Rouge, USA",
     30.4459596,
     -91.18738
   ],
   [
-    "Pennsylvania State University | State Park, USA | <a href='https://www.psice.psu.edu/ ' target='_blank'>https://www.psice.psu.edu/ </a>",
-    42.4794133,
-    -104.9702482
+    "Peking University (PKU) | Beijing, China",
+    39.906217,
+    116.3912757
+  ],
+  [
+    "Pennsylvania State University (PSU) | State College Pennsylvania, USA | <a href='https://www.psice.psu.edu/ ' target='_blank'>https://www.psice.psu.edu/ </a>",
+    40.7944504,
+    -77.8616386
   ],
   [
     "Potsdam Institute for Climate Impact Research (PIK) | Potsdam, Germany | <a href='https://www.pik-potsdam.de/ice' target='_blank'>https://www.pik-potsdam.de/ice</a>",
@@ -35,14 +45,29 @@ var addressPoints = [
     -68.672791
   ],
   [
+    "University of New South Wales (UNSW) | Sydney, Australia | <a href='http://www.essrc.unsw.edu.au/' target='_blank'>http://www.essrc.unsw.edu.au/</a>",
+    -33.8548157,
+    151.2164539
+  ],
+  [
     "University of Northern British Columbia (UNBC) | Prince George, Canada",
     53.9128645,
     -122.7453699
   ],
   [
+    "University of Tasmania (UTAS) | Hobart, Australia | <a href='https://www.imas.utas.edu.au/research/oceans-and-cryosphere' target='_blank'>https://www.imas.utas.edu.au/research/oceans-and-cryosphere</a>",
+    -42.8825088,
+    147.3281233
+  ],
+  [
     "University of Washingtion (UW) | Seattle, USA | <a href='https://earthweb.ess.washington.edu/Glaciology/ ' target='_blank'>https://earthweb.ess.washington.edu/Glaciology/ </a>",
     47.6038321,
     -122.3300624
+  ],
+  [
+    "Victoria University of Wellingtion (VUW) | Wellington, New Zealand | <a href='https://www.wgtn.ac.nz/antarctic/research/glacial-modelling' target='_blank'>https://www.wgtn.ac.nz/antarctic/research/glacial-modelling</a>",
+    -41.2887953,
+    174.7772114
   ],
   [
     "Yale University | New Haven, USA",
