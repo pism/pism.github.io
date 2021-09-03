b106fb88387055a260325a20ecafad9d96082f33
Constantine Khrulev <ckhroulev@alaska.edu>
2021-09-03 10:47:20 -0800

    Modify the plotting script to create a bar plot with Andy's papers

--- publications/plot_references.py
+++ publications/plot_references.py
@@ -8,8 +8,10 @@ from pybtex.database import parse_file, BibliographyData
 import numpy as np
 import matplotlib.pylab as plt
 import sys
+import re
 
 def get_year(bib_data, year):
+    "Return records in `bib_data` for a given `year`"
     D = {}
     for k, v in bib_data.entries.items():
         if int(v.fields["year"]) == year:
@@ -17,12 +19,16 @@ def get_year(bib_data, year):
     return BibliographyData(D)
 
 def years(bib_data):
+    "Return the set of years present in `bib_data`"
     result = []
     for _, v in bib_data.entries.items():
         result.append(int(v.fields["year"]))
     return set(result)
 
-def plot(year, no_of_pubs, output_file):
+def plot(year, no_of_pubs, output_file, aschwanden_pubs=None):
+    """Create a bar graph of `no_of_pubs` for each year in `years`, saving
+    to `output_file`"""
+
     bar_width = 0.5
     offset = bar_width
     ymax = no_of_pubs.max() + 1
@@ -32,6 +38,10 @@ def plot(year, no_of_pubs, output_file):
 
     ax.bar(year + offset, no_of_pubs,
            bar_width, color="#C6DBEF", edgecolor="#3182BD", linewidth=2.5)
+
+    if aschwanden_pubs is not None:
+        ax.bar(year + offset, aschwanden_pubs,
+               bar_width, color="#cbc9e2", edgecolor="#54278f", linewidth=2.5)
 
     ax.spines["right"].set_color("none")
     ax.spines["top"].set_color("none")
@@ -56,19 +66,39 @@ def plot(year, no_of_pubs, output_file):
     fig.savefig(output_file, bbox_inches="tight")
 
 if __name__ == "__main__":
+    # First command-line argument is the input (.bib) file, the second
+    # is the output file
     input_file = sys.argv[1]
     output_file = sys.argv[2]
+    aschwanden_file = sys.argv[2]
 
+    # parse the .bib file
     data = parse_file(input_file)
 
+    # get a list of years in the .bib file
     Y = years(data)
 
+    # Count the number of publications for each year in the range of
+    # years in `Y` above. We do this to keep the years that have 0
+    # publications.
     N = max(Y) - min(Y) + 1
     year = np.linspace(min(Y), max(Y), N, dtype=int)
     no_of_pubs = np.zeros_like(year)
 
+    aschwanden_pubs = np.zeros_like(year)
     for k in range(N):
         year_data = get_year(data, year[k])
         no_of_pubs[k] = len(year_data.entries)
 
+        for key, entry in year_data.entries.items():
+            authors = ", ".join([str(p) for p in entry.persons["author"]])
+            if re.match(".*Aschwanden.*", authors):
+                aschwanden_pubs[k] += 1
+
     plot(year, no_of_pubs, output_file)
+
+    try:
+        aschwanden_file = sys.argv[3]
+        plot(year, no_of_pubs, aschwanden_file, aschwanden_pubs=aschwanden_pubs)
+    except:
+        pass
