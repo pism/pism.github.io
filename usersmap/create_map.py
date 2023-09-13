2f24f90a9659bfc2518047d3886b219f1942e545
CI Builder (PISM website) <ckhroulev@alaska.edu>
2023-09-13 18:28:30 +0000

    [skip ci] Update the website

--- usersmap/create_map.py
+++ usersmap/create_map.py
@@ -11,35 +11,31 @@ def load_data(filename):
 
 def label(name, info):
     "Create a label for a marker"
-    link = f"<a href=\'{info['url']}\' target=\'_blank\'>{info['url']}</a>"
-    return f"{name} | {info['city']}, {info['country']} | {link}"
+    return f"<div style=\"text-align: center\">{name}<br><a href=\'{info['url']}\' target=\'_blank\'>{info['url']}</a></div>"
 
 def create_map(data, output_filename):
     "Create a leaflet map using information about markers provided in `data`"
     Map = folium.Map(min_lon=-180, max_lon=180, max_bounds=True)
 
     clusters = {}
+
     for name, info in data.items():
+
         location = f"{info['city']}, {info['country']}"
         if location not in clusters.keys():
             clusters[location] = MarkerCluster(name=location).add_to(Map)
-
-    for name, info in data.items():
 
         if "developer" in info.keys():
             icon = folium.map.Icon(color="green", icon="screwdriver-wrench", prefix="fa")
         else:
             icon = folium.map.Icon(color="blue")
 
-        popup = folium.map.Popup(label(name, info), max_width=250)
-
-        location = f"{info['city']}, {info['country']}"
-        cluster = clusters[location]
+        popup = folium.map.Popup(label(name, info), max_width=500)
 
         folium.Marker(location=(info["lat"], info["lon"]),
                       tooltip=name,
                       popup=popup,
-                      icon=icon).add_to(cluster)
+                      icon=icon).add_to(clusters[location])
 
     Map.save(output_filename)
 
